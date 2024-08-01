a3b163d3b4474257e62e08e41748db27943fa8a7
Brandon Davis <wowzerss90@yahoo.com>
2024-08-01 03:01:39 -0400

    Draw clock with minute and hour marks

---
+++ script.js
@@ -0,0 +1,57 @@
+const clock = () => {
+  const now = new Date();
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+
+  // Setup Canvas
+  ctx.save();
+  ctx.clearRect(0, 0, 500, 500);
+  ctx.translate(250, 250);
+  ctx.rotate(-Math.PI / 2);
+
+  // Set default styles
+  ctx.strokeStyle = "#000000";
+  ctx.fillStyle = "#f4f4f4";
+  ctx.lineWidth = 5;
+  ctx.lineCap = "round";
+
+  // Draw clock face/boarder
+  ctx.save();
+  ctx.beginPath();
+  ctx.lineWidth = 14;
+  ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
+  ctx.strokeStyle = "#800000";
+  ctx.stroke();
+  ctx.fill();
+  ctx.restore();
+
+  // Draw hour lines
+  ctx.save();
+  for (let i = 0; i < 12; i++) {
+    ctx.beginPath();
+    ctx.rotate(Math.PI / 6);
+    ctx.moveTo(100, 0);
+    ctx.lineTo(120, 0);
+    ctx.stroke();
+  }
+  ctx.restore();
+
+  // Draw minute lines
+  ctx.save();
+  ctx.lineWidth = 4;
+  ctx.strokeStyle = "blue";
+  for (let i = 0; i < 60; i++) {
+    if (i % 5 !== 0) {
+      ctx.beginPath();
+      ctx.moveTo(117, 0);
+      ctx.lineTo(120, 0);
+      ctx.stroke();
+    }
+    ctx.rotate(Math.PI / 30);
+  }
+  ctx.restore();
+
+  ctx.restore();
+};
+
+clock();
